838ac1c6864d89a15a96b28803774fa1b4439b1d
grabim09 <68747431+grabim09@users.noreply.github.com>
2023-09-21 02:17:08 +0700

    Update Homework1_V2.py

--- Homework1_V2.py
+++ Homework1_V2.py
@@ -101,12 +101,18 @@ def fasta_parser(file):
             sequence.append(seq)
     sequence_count = count
     st.write("Sequence amount inside the chosen file: " + str(sequence_count) + " Sequence")
+#     col1, col2 = st.columns([2, 3])
+#     with col1:
+#         st.write("Sequence amount inside the chosen file: " + str(sequence_count) + " Sequence")
+#         mult = st.checkbox("Show all sequence", value = False)
+#     with col2:
+#         i = st.slider("Choose sequence", 1, sequence_count, disabled = mult) - 1
 #   Show only one sequence
     if sequence_count > 1:
         col1, col2 = st.columns([1, 3])
         with col1:
-            st.write("Sequence amount inside the chosen file: " + str(sequence_count) + " Sequence")
-            mult = st.checkbox("Show all sequence")
+#             st.write("Sequence amount inside the chosen file: " + str(sequence_count) + " Sequence")
+            mult = st.checkbox("Show all sequence", value = False)
         with col2:
             i = st.slider("Choose sequence", 1, sequence_count, disabled = mult) - 1
     else:
